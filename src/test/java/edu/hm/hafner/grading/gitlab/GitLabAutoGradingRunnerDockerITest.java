bc1592dd65bac8645c23a7d3d0fb8b03c2308ec5
Ulli Hafner <ullrich.hafner@gmail.com>
2024-03-08 16:19:14 +0100

    Bump version to 1.7.0-SNAPSHOT.

--- src/test/java/edu/hm/hafner/grading/gitlab/GitLabAutoGradingRunnerDockerITest.java
+++ src/test/java/edu/hm/hafner/grading/gitlab/GitLabAutoGradingRunnerDockerITest.java
@@ -197,7 +197,7 @@ public class GitLabAutoGradingRunnerDockerITest {
     }
 
     private GenericContainer<?> createContainer() {
-        return new GenericContainer<>(DockerImageName.parse("uhafner/autograding-gitlab-action:1.6.0"));
+        return new GenericContainer<>(DockerImageName.parse("uhafner/autograding-gitlab-action:1.7.0-SNAPSHOT"));
     }
 
     private String readStandardOut(final GenericContainer<? extends GenericContainer<?>> container) throws TimeoutException {
