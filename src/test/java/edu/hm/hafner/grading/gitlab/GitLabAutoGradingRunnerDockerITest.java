70a91977caf228d71e3242c862c3313324cf0246
Ulli Hafner <ullrich.hafner@gmail.com>
2025-03-19 14:13:52 +0100

    Prepare for the next development version

--- src/test/java/edu/hm/hafner/grading/gitlab/GitLabAutoGradingRunnerDockerITest.java
+++ src/test/java/edu/hm/hafner/grading/gitlab/GitLabAutoGradingRunnerDockerITest.java
@@ -195,7 +195,7 @@ public class GitLabAutoGradingRunnerDockerITest {
     }
 
     private GenericContainer<?> createContainer() {
-        return new GenericContainer<>(DockerImageName.parse("uhafner/autograding-gitlab-action:3.2.0"));
+        return new GenericContainer<>(DockerImageName.parse("uhafner/autograding-gitlab-action:3.3.0-SNAPSHOT"));
     }
 
     private String readStandardOut(final GenericContainer<? extends GenericContainer<?>> container) throws TimeoutException {
