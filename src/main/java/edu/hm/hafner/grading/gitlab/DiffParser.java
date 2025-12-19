54785397a19808719c6f32fef84922464472b4d0
IamJannik <jannik.ohme@gmail.com>
2025-12-19 14:05:06 +0100

    Added Scope compatibility

---
+++ src/main/java/edu/hm/hafner/grading/gitlab/DiffParser.java
@@ -0,0 +1,37 @@
+package edu.hm.hafner.grading.gitlab;
+
+import org.gitlab4j.api.models.Diff;
+
+import java.util.*;
+
+public class DiffParser {
+    public static Map<String, Set<Integer>> getModifiedLines(final List<Diff> diffs) {
+        Map<String, Set<Integer>> changedLinesByFile = new HashMap<>();
+
+        for (Diff diff : diffs) {
+            Set<Integer> changedLines = new HashSet<>();
+
+            var diffText = diff.getDiff();
+            int lineNum = 0;
+            for (var line : diffText.split("\n")) {
+                if (line.startsWith("@@")) {
+                    var parts = line.split(" ");
+                    var newRange = parts[2];
+                    var rangeParts = newRange.substring(1).split(",");
+                    lineNum = Integer.parseInt(rangeParts[0]);
+                }
+                else if (line.startsWith("+") && !line.startsWith("+++")) {
+                    changedLines.add(lineNum);
+                    lineNum++;
+                }
+                else if (!line.startsWith("-")) {
+                    lineNum++;
+                }
+            }
+
+            var fileName = diff.getNewPath();
+            changedLinesByFile.put(fileName, changedLines);
+        }
+        return changedLinesByFile;
+    }
+}
